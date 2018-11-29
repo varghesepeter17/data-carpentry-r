5d82543f04912cde0ce699eba55d5b7d44f095aa
Varghese Peter <varghesepeter@gmail.com>
2018-11-29 13:46:37 +1100

    Piping

--- scripts/Seesion 3.R
+++ scripts/Seesion 3.R
@@ -17,7 +17,23 @@ filter(interviews, village=="God")
 # 2 conditions
 filter(interviews, village=="God" | village=="Chirodzo")
 filter(interviews, village=="God", rooms >2)
+
 #segue
 # doing the same thing again
 # comparing vectors. This is a roundabout way to do this
 filter(interviews, 1:nrow(interviews) %in% 1:10)
+
+# Doing multiple things together
+interviews2 <- filter(interviews, village=="God")
+interviews_god <- select(interviews2, no_membrs, years_liv)
+# many output variables like this may be a problem
+# one way to overcome this is names the outputs sames so it is overwritten
+#option 1
+interviews_god <-select(filter(interviews, village=="God"), no_membrs, years_liv)
+# problem: could be confusing
+
+#better option
+# piping in tidyverse
+# break the line only after the pipe symbol
+interviews_god <- interviews %>% filter(village=="God") %>% 
+  select (no_membrs, years_liv)
