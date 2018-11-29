c9054f7bd23a59540358a7707f8a8b382d74e759
Varghese Peter <varghesepeter@gmail.com>
2018-11-29 16:18:58 +1100

    plotts added

--- scripts/Session 4.R
+++ scripts/Session 4.R
@@ -1,1 +1,28 @@
 # Plotting
+
+library(tidyverse)
+
+interview_plot <- ggplot(data=interviews_plotting,aes(x=no_membrs, y=number_items))+
+  geom_point()
+
+# or
+
+interview_plot <- ggplot(data=interviews_plotting,aes(x=no_membrs, y=number_items))
+interview_plot+ geom_point(alpha=0.5)
+
+## grey points on top of each otehr makes the color black. So jitter is used
+
+interview_plot+ geom_jitter(alpha=0.5)
+
+interview_plot+ geom_jitter(alpha=0.5, color = 'blue')
+
+interview_plot+ geom_jitter(aes(color=village), alpha=0.5)
+
+ggplot(data=interviews_plotting, aes(x=village, y=rooms))+
+  geom_jitter(aes(color=respondent_wall_type))
+
+ggplot(data=interviews_plotting, aes(x=respondent_wall_type, y=rooms))+
+  geom_boxplot(alpha=0)+
+  geom_jitter(alpha=0.5, color='tomato')
+
+
