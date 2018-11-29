1381dcde18d73833276e1f357b0ee96c8bad9cd3
Varghese Peter <varghesepeter@gmail.com>
2018-11-29 13:19:13 +1100

    select function

--- scripts/Seesion 3.R
+++ scripts/Seesion 3.R
@@ -1,1 +1,14 @@
 # deplyr and tidyr
+# Tidyverse way to arranging and manipulating data
+
+library(tidyverse)
+interviews <- read_csv("data/SAFI_clean.csv", na="NULL")
+
+# selecta subset of columns
+# no need to put inverted commas as tidyverse takes care of it
+select(interviews, village, no_membrs, years_liv)
+# we can use : for column names in tidyverse just like 1:5 in the column number
+# the script below will select columns from village to rooms
+select(interviews, village:rooms)
+
+# filter
